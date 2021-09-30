67d125b58cad6163201eca98a0f0860f62af4691
Julian Grove <julian.grove@gmail.com>
2021-09-30 15:48:37 +0200

    Initial commit.

---
+++ shell.nix
@@ -0,0 +1,25 @@
+let nixpkgs_source = (fetchTarball https://github.com/NixOS/nixpkgs/archive/nixos-21.05.tar.gz);
+in
+{ pkgs ? import nixpkgs_source {
+    inherit system;
+  }
+, system ? builtins.currentSystem
+}:
+let
+  ghc = pkgs.haskellPackages.ghcWithPackages (ps: with ps; ([
+    cabal-install
+  ]));
+in
+pkgs.stdenv.mkDerivation {
+  name = "my-env-0";
+  buildInputs = [
+    ghc
+  ];
+  shellHook = ''
+    export LANG=C.UTF-8
+    export LC_ALL=C.UTF-8
+    # export LANG=en_US.UTF-8
+    # export LOCALE_ARCHIVE=/usr/lib/locale/locale-archive
+    eval $(egrep ^export ${ghc}/bin/ghc)
+  '';
+}
